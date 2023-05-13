a273f0b339fa6b5fe81dc08b3b6b8ace3f9743d6
Umair Azmat <umairazmat8260@gmail.com>
2023-05-13 18:39:53 +0500

    Problem 24 done

--- ProblemsSolutions/Program.cs
+++ ProblemsSolutions/Program.cs
@@ -561,7 +561,9 @@
             {
                 for (int j = 0; j < i; j++)
                 {
+
                     Console.Write("*");
+
                 }
                 Console.WriteLine();
             }
